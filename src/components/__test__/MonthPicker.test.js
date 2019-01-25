1340cb577a09b331318191bd2a02ffda64040d11
hanhotfox <2254553781@qq.com>
2019-01-25 14:33:43 +0800

    add MonthPicker-component test[special](all run)

--- src/components/__test__/MonthPicker.test.js
+++ src/components/__test__/MonthPicker.test.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import { mount } from 'enzyme'
 import MonthPicker from '../MonthPicker'
 import { items } from '../../containers/Home'
@@ -45,4 +46,23 @@ describe('test MonthPicker component', () => {
     expect(wrapper.state('isOpen')).toEqual(false)
     expect(props.onChange).toHaveBeenCalledWith(2014, 1)
   })
-}) 
+  it('after the dropdown is shown, click the document should close the dropdown', () => {
+    let eventMap = {}
+    document.addEventListener = jest.fn((event, cb) => {
+      eventMap[event] = cb
+    })
+    const wrapper = mount(<MonthPicker {...props} />)
+    wrapper.find('.dropdown-toggle').simulate('click')
+    expect(wrapper.state('isOpen')).toEqual(true)
+    expect(wrapper.find('.dropdown-menu').length).toEqual(1)
+
+    eventMap.click({
+      target: ReactDOM.findDOMNode(wrapper.instance())
+    })
+    expect(wrapper.state('isOpen')).toEqual(true)
+    eventMap.click({
+      target: document,
+    })
+    expect(wrapper.state('isOpen')).toEqual(false)
+  })
+})
